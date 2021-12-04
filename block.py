82e3b9090deb6ac5be6cd535e10b96168104c608
seker212 <seker212@gmail.com>
2021-12-04 19:37:41 +0100

    full block model

--- block.py
+++ block.py
@@ -1,19 +1,31 @@
-from datetime import datetime
-from random import randbytes
+from json import dumps, loads
+from typing import Optional
+from random import randint
 
 class Block:
     prev_hash : str
-    block_hash : str
-    time_stamp : datetime
-    salt : str
+    block_hash : Optional[str]
+    salt : int
+    guess: Optional[int]
     data : str
-
+    is_valid : Optional[bool]
+    
     def __init__(self, prev_hash:str, data:str) -> None:
         self.prev_hash = prev_hash
         self.block_hash = None
-        self.time_stamp = datetime.now()
-        self.salt = randbytes(32).hex()
+        self.salt = randint(0, 4294967295)
         self.data = data
+        self.guess = None
+        self.is_valid = None
 
-    def get_byets(self): #FIXME: add datetime conversion
-        return bytes.fromhex(self.prev_hash) + bytes.fromhex(self.salt) + self.data.encode()
+    @classmethod
+    def from_json(cls, json):
+        tmp = cls('', '')
+        tmp.__dict__ = loads(json)
+        return tmp
+
+    def to_bytes(self) -> bytes:
+        return bytes.fromhex(self.prev_hash) + self.salt.to_bytes(4, byteorder='big') + self.guess.to_bytes(4, byteorder='big') + self.data.encode()
+    
+    def to_json(self):
+        return dumps(self, default=lambda o: o.__dict__)
